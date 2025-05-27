ce3767cab8ce8d87ba43c11632dfb4db39870f8e
primito887 <mariokart88887@gmail.com>
2025-05-27 19:32:09 +0200

    play  y pause
    8646484846548881884

---
+++ main.js
@@ -0,0 +1,12 @@
+function playHim(){
+document.getElementById('sierrappy').play()
+document.getElementById('letoniappy').play()
+dpcument.getElementById('iranppy').play()
+}
+function pauseHim(){
+document.getElementById('sierraps').pause()
+document.getElementById('letoniaps').pause()
+dofuncitoncument.getElementById('iranpps').pause()
+}
+
+
